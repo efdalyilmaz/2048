c8fe16e2c1e7740bdcfc2f6fb24b05655eb1b4e7
Efdal Yılmaz <yilmazefdal@gmail.com>
2024-11-24 23:51:03 +0300

    async storage

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Game2048 from './Game';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Game2048 />
       <StatusBar style="auto" />
     </View>
   );
